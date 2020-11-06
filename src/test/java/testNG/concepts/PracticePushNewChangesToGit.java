63c2ee89b1cc2d63b8391aae32398a7635156739
azizurahmanniazai <73297523+azizurahmanniazai@users.noreply.github.com>
2020-11-05 19:42:22 -0500

    Update PracticePushNewChangesToGit.java

--- src/test/java/testNG/concepts/PracticePushNewChangesToGit.java
+++ src/test/java/testNG/concepts/PracticePushNewChangesToGit.java
@@ -5,5 +5,7 @@ public class PracticePushNewChangesToGit {
 	public void test() {
 		
 		System.out.println("This is just a practice");
+		
+		//This is just comment to show how to push changes made in github repository to eclipse
 	}
 }
